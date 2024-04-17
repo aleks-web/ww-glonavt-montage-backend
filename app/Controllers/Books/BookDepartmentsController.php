d2001f67c1f40ea8f8adfe57ae81a3f6f8939f94
/usr/bin/vim <support@beget.ru>
2024-04-17 14:27:53 +0300

    standart commit by alias

--- app/Controllers/Books/BookDepartmentsController.php
+++ app/Controllers/Books/BookDepartmentsController.php
@@ -10,8 +10,8 @@ use WWCrm\Models\BookDepartments;
 class BookDepartmentsController extends \WWCrm\Controllers\MainController {
 
     public function __invoke(Request $request, Response $response) {
-        return $this->view->render('book-departments.twig', [
-            'title' => 'Справочник департаментов (отделов)',
+        return $this->view->render('books/departments/page.twig', [
+            'title' => 'Справочник отделов',
             'current_user' => $this->WWCurrentUser->getUserObject()
         ]);
     }
