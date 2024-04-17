c0bdb2e72d1ad8a4e690740cbfa5081f2de7d2cb
/usr/bin/vim <support@beget.ru>
2024-04-17 14:29:26 +0300

    standart commit by alias

--- app/Controllers/Books/BookEquipmentsController.php
+++ app/Controllers/Books/BookEquipmentsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 class BookEquipmentsController extends \WWCrm\Controllers\MainController {
 
     public function __invoke(Request $request, Response $response) {
-        return $this->view->render('book-equipment.twig', [
+        return $this->view->render('books/equipments/page.twig', [
             'title' => 'Справочник оборудования',
             'current_user' => $this->WWCurrentUser->getUserObject()
         ]);
