b1d3600bed6e5c3624354f0e8a70c2307efb465e
/usr/bin/vim <support@beget.ru>
2024-04-23 14:07:37 +0300

    standart commit by alias

---
+++ app/Middlewares/Users/UsersMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WWCrm\Middlewares\Users;
+
+use Buki\Router\Http\Middleware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UsersMiddleware extends Middleware {
+
+    public function handle(Request $request, Response $response) {
+        $WWCrmContainer = \WWCrm\ServiceContainer::getInstance();
+
+        return $response;
+    }
+}
